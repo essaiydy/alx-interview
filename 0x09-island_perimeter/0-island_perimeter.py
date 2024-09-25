01c4cd0144715af26e0485a8481ec890cc4da7ad
essaiydy <imaneessaiydy2@gmail.com>
2024-09-25 16:40:19 -0700

    intrview

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,43 +1,37 @@
 #!/usr/bin/python3
-""" Island Perimeter """
+"""module island_perimeter"""
+# from typing import List
 
 
 def island_perimeter(grid):
-    """ return the perimeter of the island described in grid """
-    total_perimeter = 0
+    """
+    The perimeter of the island described in grid.
 
-    for i, row in enumerate(grid):
-        for j, element in enumerate(row):
-            # Check if element is land or sea
-            if (element == 0):
-                continue
+    Args:
+        grid (List(List(int))): 0 represent water, 1 represent land
 
-            # Left check
-            if (j != 0 and row[j - 1] == 0):
-                total_perimeter += 1
-            if (j == 0):
-                # left edge case
-                total_perimeter += 1
+    Return (int): the perimeter of the island.
+    """
 
-            # Right check
-            if (j != len(row) - 1 and row[j + 1] == 0):
-                total_perimeter += 1
-            if (j == len(row) - 1):
-                # right edge case
-                total_perimeter += 1
+    perimeter = 0
 
-            # Upper check
-            if (i != 0 and grid[i - 1][j] == 0):
-                total_perimeter += 1
-            if (i == 0):
-                # top edge case
-                total_perimeter += 1
+    for row in range(len(grid)):
+        for column in range(len(grid[0])):
+            if grid[row][column]:
 
-            # Bottom Check
-            if (i != len(grid) - 1 and grid[i + 1][j] == 0):
-                total_perimeter += 1
-            if (i == len(grid) - 1):
-                # bottom edge case
-                total_perimeter += 1
+                # check left.
+                if not column or not grid[row][column - 1]:
+                    perimeter += 1
 
-    return total_perimeter
+                # check up
+                if not row or not grid[row - 1][column]:
+                    perimeter += 1
+
+                # check right
+                if column == len(grid[0]) - 1 or not grid[row][column + 1]:
+                    perimeter += 1
+
+                # check down
+                if row == len(grid) - 1 or not grid[row + 1][column]:
+                    perimeter += 1
+    return perimeter
